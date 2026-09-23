6943fbfb5d7e79feb0de648ce65efa7af6494a76
VenkataHemaSai <venkatahemasaibhimineni@gmail.com>
2026-09-23 23:47:29 +0530

    Made book search both title and author

--- app/services/books.py
+++ app/services/books.py
@@ -2,7 +2,7 @@
 from typing import Optional
 
 from fastapi import HTTPException
-from sqlalchemy import select
+from sqlalchemy import select, or_
 from sqlalchemy.orm import Session
 
 from app.models import Book
@@ -64,7 +64,7 @@ def list_books(
     """
     query = select(Book)
     if q:
-        query = query.where(Book.title.icontains(q, autoescape=True))
+        query = query.where(or_(Book.title.icontains(q, autoescape=True), Book.author.icontains(q, autoescape=True)))
     if restricted is not None:
         query = query.where(Book.restricted == restricted)
     # TODO: min_price / max_price filters
